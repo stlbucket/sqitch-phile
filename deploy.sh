b2864cb4db92615aea6180686d357e8b54a6c12b
stlbucket <stlbucket@gmail.com>
2018-04-09 15:11:58 -0700

    engine

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-(cd ./schema/app-roles/ && sqitch deploy $1  && sqitch verify $1)
-(cd ./schema/auth/ && sqitch deploy $1  && sqitch verify $1)
-(cd ./schema/org/ && sqitch deploy $1  && sqitch verify $1)
+(cd ./schema/app-roles/ && sqitch deploy $1)
+(cd ./schema/auth/ && sqitch deploy $1)
+(cd ./schema/org/ && sqitch deploy $1)
--- engine.sh
+++ engine.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-(cd ./schema/org/ && sqitch engine add $1)
-(cd ./schema/auth/ && sqitch engine add $1)
-(cd ./schema/app-roles/ && sqitch engine add $1)
+(cd ./schema/org/ && sqitch engine add pg $1)
+(cd ./schema/auth/ && sqitch engine add pg $1)
+(cd ./schema/app-roles/ && sqitch engine add pg $1)
